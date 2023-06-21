17c41e9eb389ec22a5a7977bb430e1bd69177c25
pigniczkimerci <pigmerci@gmail.com>
2023-06-21 14:08:18 +0200

    delte task

---
+++ ourhouseworks/src/app/shared/models/task.ts
@@ -0,0 +1,7 @@
+import { Person } from "./person";
+
+export interface Tasks {
+    date: Date;
+    name: string;
+    resperson: Person;
+}
--- ourhouseworks/src/app/views/pages/main/main.component.ts
+++ ourhouseworks/src/app/views/pages/main/main.component.ts
@@ -5,6 +5,7 @@ import { Observable, first } from 'rxjs';
 import { NavbarService } from 'src/app/shared/services/navbar.service';
 import { DatePipe } from '@angular/common';
 import { Person } from 'src/app/shared/models/person';
+import { Tasks } from 'src/app/shared/models/task';
 
 @Component({
   selector: 'app-main',
@@ -14,18 +15,18 @@ import { Person } from 'src/app/shared/models/person';
 })
 export class MainComponent implements OnInit {
   taskName!: string;
-  tasks: Observable<Task[]> | undefined;
+  tasks: Observable<Tasks[]> | undefined;
   date: Date | undefined;
   responsibleMembers!: Observable<Person[]>;
   selectedMember!: Person;
   
   allFieldsFilled: boolean = false;
 
-  tableDataSource!: Task[];
+  tableDataSource!: Tasks[];
   constructor( private firestore: AngularFirestore, private auth: AngularFireAuth, public nav: NavbarService, private datePipe: DatePipe) {  }
   
   ngOnInit(): void {
-    this.tasks = this.firestore.collectionGroup('task').valueChanges() as Observable<Task[]>;
+    this.tasks = this.firestore.collectionGroup('task').valueChanges() as Observable<Tasks[]>;
     this.tasks.subscribe((data) => {
       this.tableDataSource = data;
     });
@@ -67,5 +68,42 @@ export class MainComponent implements OnInit {
   }
   checkFieldsFilled() {
     this.allFieldsFilled = !!this.taskName && !!this.date && !!this.selectedMember;
+  }
+  deleteTask(task: Tasks) {
+    console.log(task);
+    this.auth.currentUser.then((user) => {
+      if (user && task.name) {
+        const houseCollectionRef = this.firestore.collection('house');
+        const query = houseCollectionRef.ref.where('email', '==', user.email);
+        query.get().then((querySnapshot) => {
+          if (!querySnapshot.empty) {
+            const houseId = querySnapshot.docs[0].id;
+            const peopleCollectionRef = houseCollectionRef.doc(houseId).collection('task');
+            const personQuery = peopleCollectionRef.ref.where('name', '==', task.name);
+            
+            personQuery.get().then((personQuerySnapshot) => {
+              if (!personQuerySnapshot.empty) {
+                const personDocId = personQuerySnapshot.docs[0].id;
+                const personDocRef = peopleCollectionRef.doc(personDocId);
+                
+                personDocRef.delete().then(() => {
+                  console.log('Task deleted successfully from Firestore.');
+                }).catch((error) => {
+                  console.error('Error deleting task from Firestore: ', error);
+                });
+              } else {
+                console.log('Task not found in Firestore.');
+              }
+            }).catch((error) => {
+              console.error('Error retrieving task from Firestore: ', error);
+            });
+          } else {
+            console.log('House not found for the user.');
+          }
+        }).catch((error) => {
+          console.error('Error retrieving house from Firestore: ', error);
+        });
+      }
+    });
   }
 }
