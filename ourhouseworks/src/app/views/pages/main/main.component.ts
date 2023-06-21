3becbea643000344fbbd41a14ad18ba00770978d
pigniczkimerci <pigmerci@gmail.com>
2023-06-21 12:22:38 +0200

    task  with date and respmember

--- ourhouseworks/src/app/views/pages/main/main.component.ts
+++ ourhouseworks/src/app/views/pages/main/main.component.ts
@@ -3,12 +3,13 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable, first } from 'rxjs';
 import { NavbarService } from 'src/app/shared/services/navbar.service';
-
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
+  providers: [DatePipe]
 })
 export class MainComponent implements OnInit {
   taskName!: string;
@@ -16,18 +17,19 @@ export class MainComponent implements OnInit {
   tasks: Observable<any[]> | undefined;
   date: any;
   responsibleMembers!: Observable<any[]>;
-  constructor( private firestore: AngularFirestore, private auth: AngularFireAuth, public nav: NavbarService) {  }
+  selectedMember!: any;
+  constructor( private firestore: AngularFirestore, private auth: AngularFireAuth, public nav: NavbarService, private datePipe: DatePipe) {  }
   
   ngOnInit(): void {
     this.tasks = this.firestore.collectionGroup('task').valueChanges();
-    this.responsibleMembers = this.firestore.collection('people').valueChanges();
+    this.responsibleMembers = this.firestore.collectionGroup('people').valueChanges();
     this.nav.show();
   }
 
   createTask() {
     this.auth.currentUser.then((user) => {
       if (user && this.taskName) {
-        const task = {name: this.taskName};
+        const task = {name: this.taskName, date: this.date, resperson: this.selectedMember };
         this.firestore
           .collection('house', (ref) => ref.where('email', '==', user.email))
           .get()
