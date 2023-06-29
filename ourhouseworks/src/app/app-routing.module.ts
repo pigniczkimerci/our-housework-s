65a0f3b092750905e61c5a0bbbc3cf25a483ba55
pigniczkimerci <pigmerci@gmail.com>
2023-06-29 13:33:21 +0200

    sidenav and toolbar fix

--- ourhouseworks/src/app/app-routing.module.ts
+++ ourhouseworks/src/app/app-routing.module.ts
@@ -3,20 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './views/pages/login/login.component';
 import { RegisterComponent } from './views/pages/register/register.component';
 import { MainComponent } from './views/pages/main/main.component';
-import { AuthGuard } from './shared/auth/auth.guard';
 import { ProfilesComponent } from './views/pages/profiles/profiles.component';
+import { AuthGuard } from './shared/auth/auth.guard';
 import { SidenavComponent } from './shared/component/sidenav/sidenav.component';
 
 const routes: Routes = [
-  {path:"", component: LoginComponent},
-  {path:"home", component:LoginComponent},
-  {path:"login", component:LoginComponent},
-  {path: "register", component:RegisterComponent},
-  {path: "main", component:MainComponent, canActivate: [AuthGuard]},
-  {path: "profile", component:ProfilesComponent, canActivate: [AuthGuard]}
-  
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'main', component: MainComponent , canActivate: [AuthGuard]},
+  { path: 'profile', component: ProfilesComponent , canActivate: [AuthGuard]},
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
 ];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
