9e7662c1c0a0e086bb28794c7cd1819a8b427c2b
pigniczkimerci <pigmerci@gmail.com>
2023-06-29 17:30:11 +0200

    update tasks

--- ourhouseworks/src/app/shared/models/task.ts
+++ ourhouseworks/src/app/shared/models/task.ts
@@ -1,6 +1,8 @@
 import { Person } from "./person";
 
 export interface Tasks {
+    id: string;
+    isEditing: boolean;
     date: Date;
     name: string;
     resperson: Person;
--- ourhouseworks/src/app/views/pages/main/main.component.ts
+++ ourhouseworks/src/app/views/pages/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable, first } from 'rxjs';
@@ -22,16 +22,25 @@ export class MainComponent implements OnInit {
   
   allFieldsFilled: boolean = false;
 
-  tableDataSource!: Tasks[];
-  constructor( private firestore: AngularFirestore, private auth: AngularFireAuth, public nav: NavbarService, private datePipe: DatePipe) {  }
+  tableDataSource: Tasks[] = [];
+
+  updatedTask!: Tasks;
+  taskID!: any;
+  constructor(private cdRef: ChangeDetectorRef, private firestore: AngularFirestore, private auth: AngularFireAuth, public nav: NavbarService, private datePipe: DatePipe) {  }
   
   ngOnInit(): void {
     this.tasks = this.firestore.collectionGroup('task').valueChanges() as Observable<Tasks[]>;
     this.tasks.subscribe((data) => {
       this.tableDataSource = data;
-    });
+      this.tableDataSource.forEach((element) => {
+        element.isEditing = false;
+      });
+    })
     this.responsibleMembers = this.firestore.collectionGroup('people').valueChanges() as Observable<Person[]>;
     this.nav.show();
+   
+    
+    this.cdRef.detectChanges();
   }
 
   createTask() {
@@ -105,5 +114,52 @@ export class MainComponent implements OnInit {
         });
       }
     });
+  }
+
+  editTask(task: Tasks) {
+    this.auth.currentUser.then((user) => {
+      if (user) {
+        const houseCollectionRef = this.firestore.collection('house');
+        const query = houseCollectionRef.ref.where('email', '==', user.email);
+        query.get().then((querySnapshot) => {
+          if (!querySnapshot.empty) {
+            const houseId = querySnapshot.docs[0].id;
+            const tasksCollectionRef = houseCollectionRef.doc(houseId).collection('task');
+            const taskQuery = tasksCollectionRef.ref.where('name', '==', task.name)
+                                            .where('resperson', '==', task.resperson)
+                                            .where('date', '==', task.date);
+              taskQuery.get().then((taskQuerySnapshot) => {
+              if (!taskQuerySnapshot.empty) {
+                const personDocId = taskQuerySnapshot.docs[0].id;
+                const personDocRef = tasksCollectionRef.doc(personDocId);
+                this.taskID = personDocRef;
+              } else {
+               console.log('Task not found in Firestore.');
+             }
+            }).catch((error) => {
+              console.error('Error retrieving task from Firestore: ', error);
+            });
+          } else {
+            console.log('House not found for the user.');
+          }
+        }).catch((error) => {
+          console.error('Error retrieving house from Firestore: ', error);
+        });
+      }
+    });
+    task.isEditing = true;
+  }
+  
+  cancelEditTask(task: Tasks) {
+    task.isEditing = false;
+  }
+  
+  updateTask(task: Tasks) {
+    console.log(this.taskID);
+    this.taskID.update({  name: task.name, resperson: task.resperson, date: task.date }).then(() => {
+        task.isEditing = false; // Exit edit mode
+      }).catch((error:any) => {
+      console.error('Error updating task name in Firestore: ', error);
+    });        
   }
 }
