e0f5f999f3f9021f6b5604adf692c9eb916323f6
pigniczkimerci <pigmerci@gmail.com>
2023-06-30 13:38:44 +0200

    optimalize datbase service

--- ourhouseworks/src/app/shared/services/database.service.ts
+++ ourhouseworks/src/app/shared/services/database.service.ts
@@ -10,172 +10,106 @@ import { Tasks } from '../models/task';
 export class DatabaseService {
 
   constructor(private firestore: AngularFirestore,private auth: AngularFireAuth) { }
-//CREATE
-  addTaskToFirestore(taskName: string, date: any, selectedMember: Person): Promise<void> {
-    return new Promise<void>((resolve, reject) => {
-      this.auth.currentUser.then((user) => {
-        if (user && taskName) {
-          const task = { taskName: taskName, date: date, resperson: selectedMember };
-          this.firestore
-            .collection('house', (ref) => ref.where('email', '==', user.email))
-            .get()
-            .toPromise()
-            .then((querySnapshot) => {
-              if (!querySnapshot!.empty) {
-                const houseId = querySnapshot!.docs[0].id;
-                this.firestore
-                  .collection('house')
-                  .doc(houseId)
-                  .collection('task')
-                  .add(task)
-                  .then(() => {
-                    console.log('Task added successfully to Firestore.');
-                    resolve();
-                  })
-                  .catch((error) => {
-                    console.error('Error adding task to Firestore: ', error);
-                    reject(error);
-                  });
-              } else {
-                console.log('House not found for the user.');
-                reject('House not found for the user.');
-              }
-            })
-            .catch((error) => {
-              console.error('Error retrieving house from Firestore: ', error);
-              reject(error);
-            });
+  
+  private getHouseCollectionRef(): Promise<any> {
+    return this.auth.currentUser.then((user) => {
+      return this.firestore.collection('house', (ref) => ref.where('email', '==', user!.email));
+    });
+  }
+
+  private getHouseId(): Promise<string | null> {
+    return this.getHouseCollectionRef().then((houseCollectionRef) => {
+      return houseCollectionRef.get().toPromise().then((querySnapshot: { empty: any; docs: { id: any; }[]; }) => {
+        if (!querySnapshot.empty) {
+          return querySnapshot.docs[0].id;
+        } else {
+          return null;
         }
       });
+    });
+  }
+
+  private getTaskCollectionRef(): Promise<any> {
+    return this.getHouseId().then((houseId) => {
+      if (houseId) {
+        return this.firestore.collection('house').doc(houseId).collection('task').ref;
+      } else {
+        throw new Error('House not found for the user.');
+      }
+    });
+  }
+
+  private getPersonCollectionRef(): Promise<any> {
+    return this.getHouseId().then((houseId) => {
+      if (houseId) {
+        return this.firestore.collection('house').doc(houseId).collection('people').ref;
+      } else {
+        throw new Error('House not found for the user.');
+      }
+    });
+  }
+
+  addTaskToFirestore(taskName: string, date: any, selectedMember: Person): Promise<void> {
+    return this.getTaskCollectionRef().then((taskCollectionRef) => {
+      const task = { taskName: taskName, date: date, resperson: selectedMember };
+      return taskCollectionRef.add(task);
+    }).then(() => {
+      console.log('Task added successfully to Firestore.');
+    }).catch((error) => {
+      console.error('Error adding task to Firestore: ', error);
+      throw error;
     });
   }
 
   addPersonToFirestore(personName: string): Promise<void> {
-    return new Promise<void>((resolve, reject) => {
-      this.auth.currentUser.then((user) => {
-        if (user && personName) {
-          const person = { personName: personName };
-          this.firestore
-            .collection('house', (ref) => ref.where('email', '==', user.email))
-            .get()
-            .toPromise()
-            .then((querySnapshot) => {
-              if (!querySnapshot!.empty) {
-                const houseId = querySnapshot!.docs[0].id;
-                this.firestore
-                  .collection('house')
-                  .doc(houseId)
-                  .collection('people')
-                  .add(person)
-                  .then(() => {
-                    console.log('Person added successfully to Firestore.');
-                    resolve();
-                  })
-                  .catch((error) => {
-                    console.error('Error adding person to Firestore: ', error);
-                    reject(error);
-                  });
-              } else {
-                console.log('House not found for the user.');
-                reject('House not found for the user.');
-              }
-            })
-            .catch((error) => {
-              console.error('Error retrieving person from Firestore: ', error);
-              reject(error);
-            });
-        }
-      });
+    return this.getPersonCollectionRef().then((personCollectionRef) => {
+      const person = { personName: personName };
+      return personCollectionRef.add(person);
+    }).then(() => {
+      console.log('Person added successfully to Firestore.');
+    }).catch((error) => {
+      console.error('Error adding person to Firestore: ', error);
+      throw error;
     });
   }
-  //DELETE
+
   deletePersonFromFirestore(person: Person): Promise<void> {
-    return new Promise<void>((resolve, reject) => {
-      this.auth.currentUser.then((user) => {
-        if (user && person.personName) {
-          const houseCollectionRef = this.firestore.collection('house');
-          const query = houseCollectionRef.ref.where('email', '==', user.email);
-
-          query.get().then((querySnapshot) => {
-            if (!querySnapshot.empty) {
-              const houseId = querySnapshot.docs[0].id;
-              const peopleCollectionRef = houseCollectionRef.doc(houseId).collection('people');
-              const personQuery = peopleCollectionRef.ref.where('personName', '==', person.personName);
-
-              personQuery.get().then((personQuerySnapshot) => {
-                if (!personQuerySnapshot.empty) {
-                  const personDocId = personQuerySnapshot.docs[0].id;
-                  const personDocRef = peopleCollectionRef.doc(personDocId);
-
-                  personDocRef.delete().then(() => {
-                    console.log('Person deleted successfully from Firestore.');
-                    resolve();
-                  }).catch((error) => {
-                    console.error('Error deleting person from Firestore: ', error);
-                    reject(error);
-                  });
-                } else {
-                  console.log('Person not found in Firestore.');
-                  reject('Person not found in Firestore.');
-                }
-              }).catch((error) => {
-                console.error('Error retrieving person from Firestore: ', error);
-                reject(error);
-              });
-            } else {
-              console.log('House not found for the user.');
-              reject('House not found for the user.');
-            }
-          }).catch((error) => {
-            console.error('Error retrieving house from Firestore: ', error);
-            reject(error);
+    return this.getPersonCollectionRef().then((personCollectionRef) => {
+      const personQuery = personCollectionRef.where('personName', '==', person.personName);
+      return personQuery.get().then((personQuerySnapshot: { empty: any; docs: { ref: any; }[]; }) => {
+        if (!personQuerySnapshot.empty) {
+          const personDocRef = personQuerySnapshot.docs[0].ref;
+          return personDocRef.delete().then(() => {
+            console.log('Person deleted successfully from Firestore.');
           });
+        } else {
+          console.log('Person not found in Firestore.');
+          throw new Error('Person not found in Firestore.');
         }
       });
+    }).catch((error) => {
+      console.error('Error deleting person from Firestore: ', error);
+      throw error;
     });
   }
+
   deleteTaskFromFirestore(task: Tasks): Promise<void> {
-    return new Promise<void>((resolve, reject) => {
-      this.auth.currentUser.then((user) => {
-        if (user && task.taskName) {
-          const houseCollectionRef = this.firestore.collection('house');
-          const query = houseCollectionRef.ref.where('email', '==', user.email);
-          query.get().then((querySnapshot) => {
-            if (!querySnapshot.empty) {
-              const houseId = querySnapshot.docs[0].id;
-              const taskCollectionRef = houseCollectionRef.doc(houseId).collection('task');
-              const taskQuery = taskCollectionRef.ref.where('taskName', '==', task.taskName);
-
-              taskQuery.get().then((taskQuerySnapshot) => {
-                if (!taskQuerySnapshot.empty) {
-                  const taskDocId = taskQuerySnapshot.docs[0].id;
-                  const taskDocRef = taskCollectionRef.doc(taskDocId);
-
-                  taskDocRef.delete().then(() => {
-                    console.log('Task deleted successfully from Firestore.');
-                    resolve();
-                  }).catch((error) => {
-                    console.error('Error deleting task from Firestore: ', error);
-                    reject(error);
-                  });
-                } else {
-                  console.log('Task not found in Firestore.');
-                  reject('Task not found in Firestore.');
-                }
-              }).catch((error) => {
-                console.error('Error retrieving task from Firestore: ', error);
-                reject(error);
-              });
-            } else {
-              console.log('House not found for the user.');
-              reject('House not found for the user.');
-            }
-          }).catch((error) => {
-            console.error('Error retrieving house from Firestore: ', error);
-            reject(error);
+    return this.getTaskCollectionRef().then((taskCollectionRef) => {
+      const taskQuery = taskCollectionRef.where('taskName', '==', task.taskName);
+      return taskQuery.get().then((taskQuerySnapshot: { empty: any; docs: { ref: any; }[]; }) => {
+        if (!taskQuerySnapshot.empty) {
+          const taskDocRef = taskQuerySnapshot.docs[0].ref;
+          return taskDocRef.delete().then(() => {
+            console.log('Task deleted successfully from Firestore.');
           });
+        } else {
+          console.log('Task not found in Firestore.');
+          throw new Error('Task not found in Firestore.');
         }
       });
+    }).catch((error) => {
+      console.error('Error deleting task from Firestore: ', error);
+      throw error;
     });
   }
 }
